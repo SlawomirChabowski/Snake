4f64fe262524df5867b34e1d54c1ddf8bc3f3fc7
Sławek Chabowski <slawomir.chabowski@gmail.com>
2020-05-30 19:01:42 +0200

    Added missing index.html file (with mechanism of copying it to ./dist folder)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
@@ -14,6 +15,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
   devServer: {
     contentBase: './dist',
   },
